77d309ba89a9921b8bf4342d86fb73c6e1b32941
Shayan Moradi <shayanmoradi@Shayans-MacBook-Pro.local>
2023-09-08 02:43:22 +0330

    first commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -27,7 +27,7 @@ const page = () => {
         <>
             <div className='container mx-auto '>
                 <h3 className='text-3xl font-bold mb-6'>با ما در ارتباط باشید</h3>
-                <div className="contact-list grid grid-cols-3 gap-4">
+                <div className="contact-list grid grid-cols-1 md:grid-cols-3 gap-4">
                     {ContactData.map((contact) => (
                         <a key={contact.key} href={contact.link} className="contact-card group rounded-lg bg-gray-50 border-4 border-solid hover:border-primary hover:text-green-500 cursor-pointer flex flex-col justify-center items-center p-4 active:bg-primary active:text-white">
                             <span className='text-3xl m-4  group-hover:text-white group-hover:bg-primary p-4 rounded-full'>
